6d083c92022398aea856e20a96b9d9925ff3b0dd
Safal Thapaliya <safalthapaliya@gmail.com>
2024-08-01 12:38:34 +0545

    Optimizer configuration in base module

--- src/models/base_module.py
+++ src/models/base_module.py
@@ -8,7 +8,7 @@ from monai.metrics.meaniou import compute_iou
 from pytorch_lightning import LightningModule
 from pytorch_lightning.loggers.wandb import WandbLogger
 from torch import nn
-from torch.nn import functional as F    
+from torch.nn import functional as F
 
 
 _mapping_str_any = Mapping[str, Any]
@@ -44,6 +44,9 @@ class BaseModule(LightningModule):
         threshold: float = 0.5,
         multi_class: bool = True,
         log_output_masks: bool = True,
+        segmentation_lambda: float = 1,
+        classification_lambda: float = 1,
+        lr_scheduler_config:  _mapping_str_any | None = None,
     ) -> None:
         super().__init__()
 
@@ -64,13 +67,25 @@ class BaseModule(LightningModule):
         return self.net(*args, **kwargs)
 
     def losses(self, pred_mask, pred_frame_logits, gt_mask, gt_frame_types):
-        segmentation_loss = self.segmentation_criterion(pred_mask, gt_mask)
 
-        frame_num_loss = self.classification_criterion(
-            pred_frame_logits.view(-1, 3), gt_frame_types.view(-1)
+        # shape (pred_mask): B, 140, H, W -> B * 140, 1, H, W
+        # shape (gt_mask): B, 140, H, W -> B * 140, 1, H, W
+
+        B, C, H, W = pred_mask.shape
+
+        segmentation_loss = self.segmentation_criterion(
+            pred_mask.view(-1, 1, H, W), gt_mask.view(-1, 1, H, W)
         )
 
-        total_loss = segmentation_loss + frame_num_loss
+        print(f'{pred_frame_logits.dtype=}, {gt_frame_types.dtype=}')
+        frame_num_loss = self.classification_criterion(
+            pred_frame_logits.view(-1, 3), gt_frame_types.view(-1).long()
+        )
+
+        total_loss = (
+            self.hparams.segmentation_lambda * segmentation_loss
+            + self.hparams.classification_lambda * frame_num_loss
+        )
 
         return {
             "segmentation_loss": segmentation_loss,
@@ -87,10 +102,8 @@ class BaseModule(LightningModule):
     def step(self, batch: _mapping_str_any) -> _mapping_str_any:
         image, mask, frame_type = batch["image"], batch["mask"], batch["frame_type"]
 
-        pred_masks, pred_frame_logits, _ = self(
-            x=image, frame_type=frame_type
-        )
-        
+        pred_masks, pred_frame_logits = self(image)
+
         losses = self.losses(
             pred_mask=pred_masks,
             pred_frame_logits=pred_frame_logits,
@@ -103,21 +116,21 @@ class BaseModule(LightningModule):
             pred_masks=pred_masks,
         )
 
-        return out  
+        return out
 
     def training_step(self, batch: _mapping_str_any, batch_idx: int):
         out = self.step(batch)
-        images = batch['image']
-        
+        images = batch["image"]
+
         losses = dict(
-            seg_loss=out['segmentation_loss'],
+            seg_loss=out["segmentation_loss"],
             frame_num_loss=out["frame_num_loss"],
-            total_loss=out['total_loss']
+            total_loss=out["total_loss"],
         )
 
         self.log(
             "train/seg_loss",
-            losses['seg_loss'],
+            losses["seg_loss"],
             on_step=True,
             on_epoch=True,
             prog_bar=True,
@@ -125,7 +138,7 @@ class BaseModule(LightningModule):
         )
         self.log(
             "train/frame_num_loss",
-            losses['frame_num_loss'],
+            losses["frame_num_loss"],
             on_step=True,
             on_epoch=True,
             prog_bar=True,
@@ -133,7 +146,7 @@ class BaseModule(LightningModule):
         )
         self.log(
             "train/total_loss",
-            losses['total_loss'],
+            losses["total_loss"],
             on_step=True,
             on_epoch=True,
             prog_bar=True,
@@ -143,7 +156,7 @@ class BaseModule(LightningModule):
         # we can return here dict with any tensors
         # and then read it in some callback or in `training_epoch_end()` below
         # remember to always return loss from `training_step()` or backpropagation will fail!
-        return losses
+        return losses['total_loss']
 
     def on_train_epoch_end(self):
         pass
@@ -157,12 +170,12 @@ class BaseModule(LightningModule):
     def validation_step(self, batch: _mapping_str_any, batch_idx: int):
         out = self.step(batch)
 
-        images, targets = batch['image'], batch['mask']
+        images, targets = batch["image"], batch["mask"]
 
         losses = dict(
-            seg_loss=out['segmentation_loss'],
+            seg_loss=out["segmentation_loss"],
             frame_num_loss=out["frame_num_loss"],
-            total_loss=out['total_loss']
+            total_loss=out["total_loss"],
         )
 
         # Log images at the start of validation step
@@ -184,14 +197,14 @@ class BaseModule(LightningModule):
             )
             self.logger.log_image(
                 key="val/pred_mask",
-                images=list(((out['pred_masks'] > self.hparams.threshold) * 1).float())[
+                images=list(((out["pred_masks"] > self.hparams.threshold) * 1).float())[
                     :max_images_logs
                 ],
             )
 
         self.log(
             "val/seg_loss",
-            losses['seg_loss'],
+            losses["seg_loss"],
             on_step=True,
             on_epoch=True,
             prog_bar=True,
@@ -199,7 +212,7 @@ class BaseModule(LightningModule):
         )
         self.log(
             "val/frame_num_loss",
-            losses['frame_num_loss'],
+            losses["frame_num_loss"],
             on_step=True,
             on_epoch=True,
             prog_bar=True,
@@ -207,7 +220,7 @@ class BaseModule(LightningModule):
         )
         self.log(
             "val/total_loss",
-            losses['total_loss'],
+            losses["total_loss"],
             on_step=True,
             on_epoch=True,
             prog_bar=True,
@@ -233,16 +246,86 @@ class BaseModule(LightningModule):
     def predict_step(self, batch: _mapping_str_any, batch_idx: int) -> Any:
         pass
 
+    def get_optim_groups(self):
+        if self.hparams.weight_decay <= 0:  # type: ignore
+            return self.parameters()
+
+        # separate out all parameters to those that will and won't experience regularizing weight decay
+        decay = set()
+        no_decay = set()
+        whitelist_weight_modules = (nn.Linear, nn.modules.conv._ConvNd)
+        blacklist_weight_modules = (
+            nn.Embedding,
+            nn.GroupNorm,
+            nn.LayerNorm,
+            nn.modules.batchnorm._NormBase,  # Base class for batchnorm and instance norm
+        )
+        for mn, m in self.named_modules():
+            for pn, _ in m.named_parameters():
+                fpn = f"{mn}.{pn}" if mn else pn  # full param name
+                if pn.endswith("proj_weight"):
+                    # Add project weights to decay set
+                    decay.add(fpn)
+                elif pn.endswith("weight"):
+                    # random note: because named_modules and named_parameters are recursive
+                    # we will see the same tensors p many many times. but doing it this way
+                    # allows us to know which parent module any tensor p belongs to...
+                    if isinstance(m, whitelist_weight_modules):
+                        # weights of whitelist modules will be weight decayed
+                        decay.add(fpn)
+                    elif isinstance(m, blacklist_weight_modules):
+                        # weights of blacklist modules will NOT be weight decayed
+                        no_decay.add(fpn)
+                else:
+                    # all paramters except weight will not be decayed
+                    no_decay.add(fpn)
+
+        inter_params = decay & no_decay
+        if len(inter_params) != 0:
+            msg = f"parameters {inter_params} made it into both decay/no_decay sets!"
+            raise ValueError(msg)
+
+        # validate that we considered every parameter
+        param_dict = dict(self.named_parameters())
+
+        extra_params = param_dict.keys() - (decay | no_decay)
+        if len(extra_params) != 0:
+            msg = f"parameters {extra_params} were not separated into either decay/no_decay set!"
+            raise ValueError(msg)
+
+        # create the pytorch optimizer parameters
+        return [
+            {
+                "params": [param_dict[pn] for pn in decay],
+                "weight_decay": self.hparams.weight_decay,  # type: ignore
+            },
+            {
+                "params": [param_dict[pn] for pn in no_decay],
+                "weight_decay": 0.0,
+            },
+        ]
+
     def configure_optimizers(self):
         """Choose what optimizers and learning-rate schedulers to use in your optimization.
         Normally you'd need one. But in the case of GANs or similar you might have multiple.
 
-        Examples:
-            https://pytorch-lightning.readthedocs.io/en/latest/common/lightning_module.html#configure-optimizers
+        This long function is unfortunately doing something very simple and is being very defensive:
+        We are separating out all parameters of the model into two buckets: those that will experience
+        weight decay for regularization and those that won't (biases, and layernorm/embedding weights).
+        We are then returning the PyTorch optimizer object.
+
+        Examples
+        --------
+            https://lightning.ai/docs/pytorch/latest/common/lightning_module.html#configure-optimizers
+
+        :return: A dict containing the configured optimizers and learning-rate schedulers to be used for training.
+
         """
-        optimizer = self.hparams.optimizer(params=self.parameters())
-        if self.hparams.scheduler is not None:
-            scheduler = self.hparams.scheduler(optimizer=optimizer)
+        optim_groups = self.get_optim_groups()
+        optimizer = self.optimizer(optim_groups)  # type: ignore
+
+        if self.scheduler is not None:
+            scheduler = self.scheduler(optimizer=optimizer)
             return {
                 "optimizer": optimizer,
                 "lr_scheduler": {
@@ -250,8 +333,10 @@ class BaseModule(LightningModule):
                     "monitor": self.hparams.scheduler_monitor,
                     "interval": "epoch",
                     "frequency": 1,
+                    **(self.hparams.lr_scheduler_config or {}),  # type: ignore
                 },
             }
+
         return {"optimizer": optimizer}
 
 
